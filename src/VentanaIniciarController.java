32af1abeb80f3ff77a7b68f47cdc27ac67d75da2
webdesigner98everyone <luisfoguz1198@gmail.com>
2023-06-25 14:11:10 -0500

    Se realiza documentacion con formato javadoc

--- src/VentanaIniciarController.java
+++ src/VentanaIniciarController.java
@@ -30,24 +30,20 @@ public class VentanaIniciarController extends Application implements Initializab
 
     @FXML
     private SiguienteController siguienteController;
-
     @FXML
     private Button btnJugar, btnIntro, stopButton, generarletraButton, ValPalabraButton;
-
     @FXML
     private Label tiempoLabel, LetraLabel, LetraPuntaje;
-
     private List<Character> abecedario = new ArrayList<>();
-
     @FXML
     private TextField NombreTextField, ApellidoTextField, AnimalTextField, ObjetoTextField, FrutaTextField, PaisTextField;
-
     private Timeline timeline;
     private int tiempoRestante = 61;
 
+    /**
+     * @param initialize Llena la lista 'abecedario' con las letras del alfabeto
+     */
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        // Llena la lista 'abecedario' con las letras del alfabeto
         for (char c = 'A'; c <= 'Z'; c++) {
             abecedario.add(c);
         }
@@ -70,14 +66,8 @@ public class VentanaIniciarController extends Application implements Initializab
         try {
             FXMLLoader loader = new FXMLLoader(new File("C:/Desarrollos/Java/ProjectStopmania/src/Ventana Intro.fxml").toURI().toURL());
             loader.load();
-
-            // Obtén la instancia existente de VentanaIniciarController
             VentanaIniciarController siguienteController = loader.getController();
-
-            // Obtén la ventana actual
             Stage stage = (Stage) btnJugar.getScene().getWindow();
-
-            // Configura la nueva escena en la ventana
             Scene scene = new Scene(loader.getRoot());
             stage.setScene(scene);
             stage.show();
@@ -86,18 +76,17 @@ public class VentanaIniciarController extends Application implements Initializab
         }
     }
 
+    /**
+     * @param handleIntroButtonAction metodo donde se llama la siguiente ventana
+     * @param Stage instacia que Obtiene la ventana actual
+     */
     @FXML
     private void handleIntroButtonAction(ActionEvent event) {
-
         try {
             FXMLLoader loader = new FXMLLoader(new File("C:/Desarrollos/Java/ProjectStopmania/src/Ventana.fxml").toURI().toURL());
             loader.load();
             VentanaIniciarController siguienteController = loader.getController();
-
-            // Obtén la ventana actual
             Stage stage = (Stage) btnIntro.getScene().getWindow();
-
-            // Configura la nueva escena en la ventana
             Scene scene = new Scene(loader.getRoot());
             stage.setScene(scene);
             stage.show();
@@ -117,21 +106,24 @@ public class VentanaIniciarController extends Application implements Initializab
         detenerCronometro();
     }
 
+    /**
+     * @param configurarCronometro metodo donde se Crea una instancia de
+     * Timeline con una duración de 1 segundo * aleatoria
+     * @param setCycleCount Se Configura que el cronómetro se repita
+     * indefinidamente
+     * @param play Inicia el cronómetro
+     */
     private void configurarCronometro() {
-        // Crea una instancia de Timeline con una duración de 1 segundo
         timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
             tiempoRestante--;
             tiempoLabel.setText(Integer.toString(tiempoRestante));
 
             if (tiempoRestante == 0) {
-                // Detener el cronómetro cuando el tiempo llega a cero
                 detenerCronometro();
             }
         }));
-        // Configura que el cronómetro se repita indefinidamente
         timeline.setCycleCount(Timeline.INDEFINITE);
 
-        // Inicia el cronómetro
         timeline.play();
     }
 
@@ -140,16 +132,22 @@ public class VentanaIniciarController extends Application implements Initializab
         stopButton.setDisable(true);
     }
 
+    /**
+     * @param handleGeneratorLetraButtonAction metodo donde se genera la letra
+     * aleatoria
+     * @param LetraLabel donde se muestra la Salida de la letra
+     */
     @FXML
     private void handleGeneratorLetraButtonAction(ActionEvent event) {
         Random random = new Random();
         int indice = random.nextInt(abecedario.size());
         char letraAleatoria = abecedario.get(indice);
 
-        // Salida de letra
         LetraLabel.setText("Letra: " + letraAleatoria);
 
-        // Asignar la letra aleatoria como el primer carácter de los TextField
+        /**
+         * Asignar la letra aleatoria como el primer carácter de los TextField
+         */
         NombreTextField.setText(Character.toString(letraAleatoria));
         ApellidoTextField.setText(Character.toString(letraAleatoria));
         AnimalTextField.setText(Character.toString(letraAleatoria));
@@ -158,12 +156,19 @@ public class VentanaIniciarController extends Application implements Initializab
         PaisTextField.setText(Character.toString(letraAleatoria));
     }
 
-    //como primer parametro Se valida si cada TextField está vacío o si el primer carácter no coincide con la letra generada
-    //posterior se agrega una nueva verificación para asegurarse de que haya una palabra en cada TextField utilizando el método esPalabraValida()
+    /**
+     * @param handleValidatorPalabraButtonAction metodo donde se valida si cada
+     * TextField está vacío o si el primer carácter no coincide con la letra
+     * generada, Luego se agrega una nueva verificación para asegurarse de que
+     * haya una palabra en cada TextField utilizando el método esPalabraValida()
+     * @param puntaje Puntaje inicial de 600
+     * @param detenerCronometro metodo que se utiliza para detener el cronómetro
+     * @param LetraPuntaje donde se Asigna el puntaje en el Label LetraPuntaje
+     */
     @FXML
     private void handleValidatorPalabraButtonAction(ActionEvent event) {
         String letraGenerada = LetraLabel.getText().substring(7); // Obtener la letra generada sin el prefijo "Letra: "
-        int puntaje = 600; // Puntaje inicial
+        int puntaje = 600;
 
         if (!esPalabraValida(NombreTextField.getText()) || NombreTextField.getText().equalsIgnoreCase(letraGenerada)) {
             puntaje -= 100;
@@ -189,13 +194,10 @@ public class VentanaIniciarController extends Application implements Initializab
             puntaje -= 100;
         }
 
-        // Detener el cronómetro, Al llamar a detenerCronometro(), se detendrá la animación del cronómetro y se desactivará el botón "Stop" para evitar que el tiempo siga avanzando
         detenerCronometro();
 
-        // Asignar el puntaje en el Label LetraPuntaje
         LetraPuntaje.setText("Puntaje: " + puntaje);
 
-        // Mostrar la alerta correspondiente según el puntaje obtenido
         if (puntaje == 600) {
             mostrarAlertaConImagenGanador("¡Ganaste!", "Has obtenido el puntaje máximo de 600 puntos.", "C:/Desarrollos/Java/ProjectStopmania/src/img/Ganador.png");
         } else {
@@ -203,58 +205,72 @@ public class VentanaIniciarController extends Application implements Initializab
         }
     }
 
+    /**
+     * @param esPalabraValida metodo para Verificar si el texto es una palabra
+     * válida (solo letras y al menos un carácter)
+     */
     private boolean esPalabraValida(String texto) {
-        // Verificar si el texto es una palabra válida (solo letras y al menos un carácter)
         return texto.matches("[A-Za-zÁ-ÿÑñ]+");
     }
 
-    //se crea un metodo para el ganador y para el sigue intentando, luego se realiza una instancia de Alert y se configuran el título y el mensaje como antes. 
-    //Luego, se carga la imagen utilizando la ruta del archivo proporcionada en imagenPath y se crea un ImageView con la imagen cargada
+    /**
+     * @param mostrarAlertaConImagenGanador metodo ganador se realiza una
+     * instancia de Alert y se configuran el título y el mensaje como antes.
+     * @param Image, instanacia para cargar la imagen utilizada
+     * @param imageView Se configura el tamaño de la imagen
+     * @param DialogPane Se Crea un DialogPane personalizado
+     */
     private void mostrarAlertaConImagenGanador(String titulo, String mensaje, String imagenPath) {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle(titulo);
         alert.setHeaderText(null);
         alert.setContentText(mensaje);
 
-        // Cargar la imagen
         Image imagen = new Image("C:/Desarrollos/Java/ProjectStopmania/src/img/Ganador.png");
         ImageView imageView = new ImageView(imagen);
 
-        // Configurar el tamaño de la imagen
         imageView.setFitWidth(100);
         imageView.setFitHeight(100);
 
-        // Crear un DialogPane personalizado
         DialogPane dialogPane = alert.getDialogPane();
         dialogPane.setGraphic(imageView);
 
         alert.showAndWait();
     }
 
+    /**
+     * @param Cargar la imagen
+     * @param Configurar el tamaño de la imagen
+     * @param Crear un DialogPane personalizado.
+     * @param Reiniciar el cronómetro
+     */
     private void mostrarAlertaConImagenPerdedor(String titulo, String mensaje, String imagenPath) {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle(titulo);
         alert.setHeaderText(null);
         alert.setContentText(mensaje);
 
-        // Cargar la imagen
         Image imagen = new Image("C:/Desarrollos/Java/ProjectStopmania/src/img/Intentando.png");
         ImageView imageView = new ImageView(imagen);
 
-        // Configurar el tamaño de la imagen
         imageView.setFitWidth(100);
         imageView.setFitHeight(100);
 
-        // Crear un DialogPane personalizado
         DialogPane dialogPane = alert.getDialogPane();
         dialogPane.setGraphic(imageView);
 
         alert.showAndWait();
     }
 
+    /**
+     * @param Reiniciar los valores de los campos de texto
+     * @param Reiniciar la etiqueta de la letra generada
+     * @param Reiniciar Reiniciar el puntaje.
+     * @param Reiniciar el cronómetro
+     */
     @FXML
     private void handleResetButtonAction(ActionEvent event) {
-        // Reiniciar los valores de los campos de texto
+
         NombreTextField.setText("");
         ApellidoTextField.setText("");
         AnimalTextField.setText("");
@@ -262,13 +278,10 @@ public class VentanaIniciarController extends Application implements Initializab
         FrutaTextField.setText("");
         PaisTextField.setText("");
 
-        // Reiniciar la etiqueta de la letra generada
         LetraLabel.setText("Letra: ");
 
-        // Reiniciar el puntaje
         LetraPuntaje.setText("Puntaje: ");
 
-        // Reiniciar el cronómetro
         tiempoRestante = 60;
         tiempoLabel.setText(Integer.toString(tiempoRestante));
         stopButton.setDisable(false);
